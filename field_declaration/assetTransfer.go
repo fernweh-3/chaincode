aa9cda3da8f8368be1eb9235a950d0ef443073fe
yixing <leiyixing@gmail.com>
2025-06-03 14:25:09 +0100

    field-declaration

---
+++ field_declaration/assetTransfer.go
@@ -0,0 +1,23 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/v2/contractapi"
+	"github.com/fernweh-3/chaincode/field_declaration/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating timestamp chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting timestamp chaincode: %v", err)
+	}
+}
